5874652d1ffd4cae393c5c3079bb3494955ea233
R4N1M <ranim.marrakchi1@gmail.com>
2018-09-26 20:17:06 +0200

    add new porject My_app and add a docker compose to build and run all the projects

--- Api/index.js
+++ Api/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function(req, res){
-   res.send("Hello world!");
+   res.send("Hello worldpmo,jmljo!");
 });
 
 app.post('/hello', function(req, res){
